28b64f736200b431150059cba1c0a37d17346315
João Antônio Barbosa <joaobarbosadev@gmail.com>
2023-10-04 20:39:49 -0300

    fix: fixed slide stroke position

--- src/components/ui/Slide/SlideRoot.tsx
+++ src/components/ui/Slide/SlideRoot.tsx
@@ -4,7 +4,7 @@ type SlideRootProps = ComponentProps<"div">;
 
 export function SlideRoot({ children }: SlideRootProps) {
   return (
-    <div className="relative grid gap-8 md:grid-cols-2 min-[890px]:after:absolute min-[890px]:after:-right-[calc(20vw-65%)] min-[890px]:after:top-[calc(50%-100px)] min-[890px]:after:h-[200px] min-[890px]:after:w-[3px] min-[890px]:after:bg-primary">
+    <div className="relative grid gap-8 md:grid-cols-2 min-[890px]:after:absolute min-[890px]:after:-right-32 min-[890px]:after:left-0 min-[890px]:after:top-[calc(50%-100px)] min-[890px]:after:m-auto min-[890px]:after:h-[200px] min-[890px]:after:w-[3px] min-[890px]:after:bg-primary">
       {children}
     </div>
   );
